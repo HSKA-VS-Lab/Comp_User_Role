70ebfb25bc1b1631017828c181f25378437e7d43
and1kl <40032530+and1kl@users.noreply.github.com>
2019-11-12 18:34:16 +0100

    Spring Boot Project Initial

---
+++ Comp_User_Role/src/main/java/de/hska/iwi/vslab/Comp_User_Role/CompUserRoleApplication.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Comp_User_Role;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompUserRoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompUserRoleApplication.class, args);
+	}
+
+}

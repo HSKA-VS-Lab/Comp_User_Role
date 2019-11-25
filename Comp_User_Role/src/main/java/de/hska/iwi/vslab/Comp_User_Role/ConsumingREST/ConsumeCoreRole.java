cb13ad7f3d8ca5006da9dfa8fcd00b60c06dfc51
and1kl <email.andreasklaiber@gmail.com>
2019-11-25 18:37:42 +0100

    dynamische Rest Aufrufe

--- Comp_User_Role/src/main/java/de/hska/iwi/vslab/Comp_User_Role/ConsumingREST/ConsumeCoreRole.java
+++ Comp_User_Role/src/main/java/de/hska/iwi/vslab/Comp_User_Role/ConsumingREST/ConsumeCoreRole.java
@@ -1,17 +1,27 @@
 package de.hska.iwi.vslab.Comp_User_Role.ConsumingREST;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
 public class ConsumeCoreRole {
     
 
-    private String urlCoreProduct = "http://localhost:8084/role";
-    
+    //private String urlCoreProduct = "http://localhost:8084/role";
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumeCoreRole.class);
     RestTemplate restTemplate = new RestTemplate();
 
     public int getRoleIdForUser() {
-        Role role = restTemplate.getForObject(urlCoreProduct + "/user", Role.class);
-        return role.getId();
+        try {
+            UrlBuilder urlBuilder = new UrlBuilder();
+            log.info("URL:" + urlBuilder.getRoleUrl_user());
+            Role role = restTemplate.getForObject(urlBuilder.getRoleUrl_user(), Role.class);
+            return role.getId();
+        } catch (Exception e) {
+            System.out.println(e);
+            throw e;
+        }
     }
 
 
--- Comp_User_Role/src/main/java/de/hska/iwi/vslab/Comp_User_Role/ConsumingREST/ConsumeCoreUser.java
+++ Comp_User_Role/src/main/java/de/hska/iwi/vslab/Comp_User_Role/ConsumingREST/ConsumeCoreUser.java
@@ -1,22 +1,39 @@
 package de.hska.iwi.vslab.Comp_User_Role.ConsumingREST;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
 
 public class ConsumeCoreUser {
 
-    private String urlCoreUser = "http://localhost:8083/user";
-    
+    //private String urlCoreUser = "http://localhost:8083/user";
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumeCoreUser.class);
     RestTemplate restTemplate = new RestTemplate();
 
 
     public void addUser(String firstname, String lastname, String username, String password, int roleId) {
-        User user = new User(firstname, lastname, username, password, roleId);
-        restTemplate.postForLocation(urlCoreUser, user);
+        try {
+            UrlBuilder urlBuilder = new UrlBuilder();
+            log.info("URL:" + urlBuilder.getUserUrl());
+            User user = new User(firstname, lastname, username, password, roleId);
+            restTemplate.postForLocation(urlBuilder.getUserUrl(), user);
+        } catch (Exception e) {
+            System.out.println(e);
+            throw e;
+        }
     }
 
     public User[] getUsers() {
-        return restTemplate.getForObject(urlCoreUser, User[].class);
+        try {
+            UrlBuilder urlBuilder = new UrlBuilder();
+            log.info("URL:" + urlBuilder.getUserUrl());
+            return restTemplate.getForObject(urlBuilder.getUserUrl(), User[].class);
+        } catch (Exception e) {
+            System.out.println(e);
+            throw e;
+        }
     }
 
 }

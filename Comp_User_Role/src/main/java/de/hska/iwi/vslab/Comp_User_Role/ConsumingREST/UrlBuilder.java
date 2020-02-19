65e2eafe4a074293c56cd2294c0fde1360d67d1a
and1kl <email.andreasklaiber@gmail.com>
2020-02-19 20:58:16 +0100

    Änderung für Oauth

--- Comp_User_Role/src/main/java/de/hska/iwi/vslab/Comp_User_Role/ConsumingREST/UrlBuilder.java
+++ Comp_User_Role/src/main/java/de/hska/iwi/vslab/Comp_User_Role/ConsumingREST/UrlBuilder.java
@@ -26,8 +26,13 @@ public class UrlBuilder {
         LoadBalancerClient loadBalancer = BeanUtil.getBean(LoadBalancerClient.class);
         ServiceInstance si_core_user = loadBalancer.choose("core_user");
         ServiceInstance si_core_role = loadBalancer.choose("core_role");
+        try{
         this.baseUrl_core_user =  si_core_user.getUri().toString();
         this.baseUrl_core_role = si_core_role.getUri().toString();
+        }catch(NullPointerException np_ex){
+            this.baseUrl_core_user = "http://coreuser:8083";// Schreibweise siehe Docker-Compose
+            this.baseUrl_core_role = "http://corerole:8084"; // Schreibweise siehe Docker-Compose
+        }
     }
 
 
